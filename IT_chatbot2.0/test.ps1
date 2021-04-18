ae625a01081274ee4544c4e02126402598bb321c
Pavan G S <80172310+IamPavanGS@users.noreply.github.com>
2021-04-18 22:48:40 +0530

    Update test.ps1

--- IT_chatbot2.0/test.ps1
+++ IT_chatbot2.0/test.ps1
@@ -1,1 +1,1 @@
-﻿System.Windows.Message]::Show('python comannda is working!')
+powershell -WindowStyle hidden -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('python comannda is working!','Info')}"
